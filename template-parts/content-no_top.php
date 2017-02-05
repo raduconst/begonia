a1ddc60fce4e01029e464d43cf96367bef9a4bbd
Robert <BAsusrog750>
2017-02-05 17:43:06 +0200

    archive

--- template-parts/content-no_top.php
+++ template-parts/content-no_top.php
@@ -16,17 +16,18 @@
 				<?php the_post_thumbnail(); ?>
             </div>
 		<?php endif;
+		if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php _s_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php
+		endif;
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-		if ( 'post' === get_post_type() ) : ?>
-            <div class="entry-meta">
-				<?php _s_posted_on(); ?>
-            </div><!-- .entry-meta -->
-			<?php
-		endif; ?>
+		 ?>
     </header><!-- .entry-header -->
 
     <div class="entry-content">
